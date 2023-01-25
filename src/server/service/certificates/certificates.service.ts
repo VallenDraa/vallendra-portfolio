d3772a9c8a37b7d2b5908f26730faa4d037ebf2a
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-01-25 16:33:37 +0700

    fix(service): changed code behaviour for querying next and previous item for the first/last project or certificate

--- src/server/service/certificates/certificates.service.ts
+++ src/server/service/certificates/certificates.service.ts
@@ -1,3 +1,4 @@
+import { LeanDocument } from "mongoose";
 import Certificate from "../../../interfaces/certificate.interface";
 import CertificateModel from "../../model/certificate.model";
 import connectMongo from "../../mongo/mongodb";
@@ -6,25 +7,38 @@ export async function getCertificateWithPrevAndNext(slug: string) {
   try {
     connectMongo();
 
-    const certificate = (await CertificateModel.findOne({
+    let nextCertificate: LeanDocument<Certificate>,
+      prevCertificate: LeanDocument<Certificate>;
+    const certificate = await CertificateModel.findOne<Certificate>({
       slug,
-    }).lean()) as Certificate;
+    }).lean();
 
-    /* get the next and previous Certificate name and slug
+    if (certificate === null) throw new Error();
+    if (!certificate.createdAt) throw new Error();
+
+    const certificateId = certificate._id.toString();
+    const certificateCreatedAt = certificate.createdAt as Date;
+
+    const { _id: firstCertificateId } = await getFirstCertificate(["_id"]);
+    const { _id: lastCertificateId } = await getLastCertificate(["_id"]);
+
+    const certificateIsFirst = certificateId === firstCertificateId.toString();
+    const certificateIsLast = certificateId === lastCertificateId.toString();
+
+    /* get the next and previous certificate name and slug
     ================================================= */
-    const [nextCertificate] = await CertificateModel.find({
-      createdAt: { $gt: certificate.createdAt },
-    })
-      .sort({ createdAt: 1 })
-      .limit(1)
-      .select(["slug", "name"]);
-
-    const [prevCertificate] = await CertificateModel.find({
-      createdAt: { $lt: certificate.createdAt },
-    })
-      .sort({ createdAt: -1 })
-      .limit(1)
-      .select(["slug", "name"]);
+    if (certificateIsFirst) {
+      nextCertificate = await getNextCertificate(certificateCreatedAt);
+      prevCertificate = await getLastCertificate(["slug", "name"]);
+    } else {
+      if (certificateIsLast) {
+        prevCertificate = await getPrevCertificate(certificateCreatedAt);
+        nextCertificate = await getFirstCertificate(["slug", "name"]);
+      } else {
+        prevCertificate = await getPrevCertificate(certificateCreatedAt);
+        nextCertificate = await getNextCertificate(certificateCreatedAt);
+      }
+    }
 
     return JSON.stringify({
       certificate,
@@ -46,4 +60,50 @@ export async function getAllCertificates() {
   } catch (error) {
     console.error(error);
   }
+}
+
+/* Helpers
+========== */
+async function getFirstCertificate(select: (keyof Certificate)[] = []) {
+  const [firstCertificate] = await CertificateModel.find<Certificate>()
+    .sort({ createdAt: 1 })
+    .limit(1)
+    .select(select)
+    .lean();
+
+  return firstCertificate;
+}
+
+async function getLastCertificate(select: (keyof Certificate)[] = []) {
+  const [lastCertificate] = await CertificateModel.find<Certificate>()
+    .sort({ createdAt: -1 })
+    .limit(1)
+    .select(select)
+    .lean();
+
+  return lastCertificate;
+}
+
+async function getNextCertificate(createdAt: Date) {
+  const [nextCertificate] = await CertificateModel.find<Certificate>({
+    createdAt: { $gt: createdAt },
+  })
+    .sort({ createdAt: 1 })
+    .limit(1)
+    .select(["slug", "name"])
+    .lean();
+
+  return nextCertificate;
+}
+
+async function getPrevCertificate(createdAt: Date) {
+  const [prevCertificate] = await CertificateModel.find<Certificate>({
+    createdAt: { $lt: createdAt },
+  })
+    .sort({ createdAt: -1 })
+    .limit(1)
+    .select(["slug", "name"])
+    .lean();
+
+  return prevCertificate;
 }
--- src/server/service/projects/projects.service.ts
+++ src/server/service/projects/projects.service.ts
@@ -1,3 +1,4 @@
+import { LeanDocument } from "mongoose";
 import { Project } from "../../../interfaces/project.interface";
 import ProjectModel from "../../model/project.model";
 import connectMongo from "../../mongo/mongodb";
@@ -16,23 +17,36 @@ export async function getTopPickedProjects() {
 export async function getProjectWithPrevAndNext(slug: string) {
   try {
     connectMongo();
-    const project = (await ProjectModel.findOne({ slug }).lean()) as Project;
+
+    let nextProject: LeanDocument<Project>, prevProject: LeanDocument<Project>;
+    const project = await ProjectModel.findOne<Project>({ slug }).lean();
+
+    if (project === null) throw new Error();
+    if (!project.createdAt) throw new Error();
+
+    const projectId = project._id.toString();
+    const projectCreatedAt = project.createdAt as Date;
+
+    const { _id: firstProjectId } = await getFirstProject(["_id"]);
+    const { _id: lastProjectId } = await getLastProject(["_id"]);
+
+    const projectIsFirst = projectId === firstProjectId.toString();
+    const projectIsLast = projectId === lastProjectId.toString();
 
     /* get the next and previous project name and slug
     ================================================= */
-    const [nextProject] = await ProjectModel.find({
-      createdAt: { $gt: project.createdAt },
-    })
-      .sort({ createdAt: 1 })
-      .limit(1)
-      .select(["slug", "name"]);
-
-    const [prevProject] = await ProjectModel.find({
-      createdAt: { $lt: project.createdAt },
-    })
-      .sort({ createdAt: -1 })
-      .limit(1)
-      .select(["slug", "name"]);
+    if (projectIsFirst) {
+      nextProject = await getNextProject(projectCreatedAt);
+      prevProject = await getLastProject(["slug", "name"]);
+    } else {
+      if (projectIsLast) {
+        prevProject = await getPrevProject(projectCreatedAt);
+        nextProject = await getFirstProject(["slug", "name"]);
+      } else {
+        prevProject = await getPrevProject(projectCreatedAt);
+        nextProject = await getNextProject(projectCreatedAt);
+      }
+    }
 
     return JSON.stringify({
       project,
@@ -54,4 +68,50 @@ export async function getAllProjects() {
   } catch (error) {
     console.error(error);
   }
+}
+
+/* Helpers
+========== */
+async function getFirstProject(select: (keyof Project)[] = []) {
+  const [firstProject] = await ProjectModel.find<Project>()
+    .sort({ createdAt: 1 })
+    .limit(1)
+    .select(select)
+    .lean();
+
+  return firstProject;
+}
+
+async function getLastProject(select: (keyof Project)[] = []) {
+  const [lastProject] = await ProjectModel.find<Project>()
+    .sort({ createdAt: -1 })
+    .limit(1)
+    .select(select)
+    .lean();
+
+  return lastProject;
+}
+
+async function getNextProject(createdAt: Date) {
+  const [nextProject] = await ProjectModel.find<Project>({
+    createdAt: { $gt: createdAt },
+  })
+    .sort({ createdAt: 1 })
+    .limit(1)
+    .select(["slug", "name"])
+    .lean();
+
+  return nextProject;
+}
+
+async function getPrevProject(createdAt: Date) {
+  const [prevProject] = await ProjectModel.find<Project>({
+    createdAt: { $lt: createdAt },
+  })
+    .sort({ createdAt: -1 })
+    .limit(1)
+    .select(["slug", "name"])
+    .lean();
+
+  return prevProject;
 }
