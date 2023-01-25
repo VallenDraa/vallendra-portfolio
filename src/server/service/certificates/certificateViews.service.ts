143314439bd86ff243a25a515ee9b21b958f6458
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-01-25 21:38:07 +0700

    feat: added starter code for the certificate views and likes service

--- src/server/service/certificates/certificateViews.service.ts
+++
@@ -1,3 +0,0 @@
-export async function getCertificateViews() {}
-
-export async function getAllCertificateViews() {}
---
+++ src/server/service/certificates/certificateViewsAndLikes.service.ts
@@ -0,0 +1,3 @@
+export async function getCertificateViewsAndLikes() {}
+
+export async function getAllCertificateViewsAndLikes() {}
