d5e48173569433e6324f9702170ea580b63cc298
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-01-26 13:59:04 +0700

    feat: added new json serialize helper

---
+++ src/utils/server/serialize.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param arg any type of argument is okay
+ * @returns returns the same value as the argument but it is json serializable
+ */
+
+export async function JSONSerialize<T>(arg: T) {
+  const temp = JSON.stringify(arg);
+  const serialized = JSON.parse(temp);
+
+  return serialized as T;
+}
