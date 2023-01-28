27b780ff5ab8669abd0b3b4b3e93705c791eb511
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-01-28 15:58:54 +0700

    fix: made sure term is correct after logging in to comment

--- src/components/DetailsPage/Comment.tsx
+++ src/components/DetailsPage/Comment.tsx
@@ -1,14 +1,23 @@
 import Giscus, { Repo } from "@giscus/react";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
+import R from "react";
 
 export default function Comment() {
-  const { asPath } = useRouter();
+  const { pathname, query } = useRouter();
+  const term = R.useMemo(() => {
+    const splitPathname = pathname.split("/");
+    splitPathname[2] = query.slug as string;
+
+    const result = splitPathname.join("/");
+
+    return result;
+  }, [pathname, query]);
   const { theme } = useTheme();
 
   return (
     <Giscus
-      term={asPath}
+      term={term}
       repo={(process.env.NEXT_PUBLIC_REPO as Repo) || ""}
       repoId={process.env.NEXT_PUBLIC_REPOID || ""}
       category="Comments"
