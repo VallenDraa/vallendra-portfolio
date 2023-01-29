132b963993590f08d7ef1974cec05e1abd4a43ec
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-01-29 10:30:01 +0700

    feat: added share API support

--- src/components/DetailsPage/CopyLinkBtn.tsx
+++ src/components/DetailsPage/CopyLinkBtn.tsx
@@ -1,60 +1,116 @@
-import { Button } from "@material-tailwind/react";
-import { useState } from "react";
+import R from "react";
 import Show from "../../utils/client/jsx/Show";
-import {
-  AiOutlineCheck,
-  AiOutlineClose,
-  AiOutlineGlobal,
-  AiOutlineLink,
-} from "react-icons/ai";
+import { AiOutlineCheck, AiOutlineClose, AiOutlineLink } from "react-icons/ai";
 import ActionButton from "../StyledComponents/ActionButton";
 
 export default function CopyLinkBtn() {
-  const [hasBeenPressed, setHasBeenPressed] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [shareIsSupported, setShareIsSupported] = R.useState<boolean | null>(
+    null
+  );
+  const [copyLinkIsSupported, setCopyLinkIsSupported] = R.useState<
+    boolean | null
+  >();
+  null;
+
+  const [hasBeenPressed, setHasBeenPressed] = R.useState(false);
+  const [isError, setIsError] = R.useState(false);
+
+  R.useEffect(() => setCopyLinkIsSupported(!!navigator.clipboard?.writeText));
+
+  R.useEffect(() => setShareIsSupported(!!navigator.share), []);
 
   function copyLinkToClipBoard() {
-    setIsError(false);
+    navigator.clipboard
+      ?.writeText(window.location.href)
+      .then(() => {
+        setHasBeenPressed(true);
+        setTimeout(() => setHasBeenPressed(false), 1500);
+      })
+      .catch(() => {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 1500);
+      });
+  }
 
-    try {
-      navigator.clipboard.writeText(window.location.href);
-      setHasBeenPressed(true);
-
-      setTimeout(() => setHasBeenPressed(false), 1500);
-    } catch (e) {
-      setIsError(true);
-    }
+  function shareInfo() {
+    navigator
+      .share({
+        title: globalThis.document.title,
+        url: globalThis.window.location.href,
+      })
+      .then(() => setHasBeenPressed(true))
+      .catch((error) => {
+        console.error(error);
+        setIsError(true);
+        setTimeout(() => setIsError(false), 1500);
+      })
+      .finally(() => setHasBeenPressed(false));
   }
 
   return (
-    <ActionButton
-      onClick={copyLinkToClipBoard}
-      color={isError ? "red" : "teal"}
-      icon={
-        <>
-          <Show when={!hasBeenPressed}>
-            <AiOutlineLink className="text-teal-500" />
-          </Show>
+    <>
+      <Show when={copyLinkIsSupported === null && shareIsSupported === null}>
+        <div className="h-9 w-full animate-pulse rounded bg-white/20 py-2 px-4 shadow-sm hover:shadow" />
+      </Show>
+
+      <Show when={copyLinkIsSupported === true && shareIsSupported === false}>
+        <ActionButton
+          className="animate-fade-in"
+          onClick={copyLinkToClipBoard}
+          color={isError ? "red" : "teal"}
+          icon={
+            <>
+              <Show when={!hasBeenPressed && !isError}>
+                <AiOutlineLink className="text-teal-500" />
+              </Show>
+
+              {/* content for pressed and errorless state */}
+              <Show when={hasBeenPressed && !isError}>
+                <AiOutlineCheck />
+              </Show>
+
+              <Show when={hasBeenPressed && isError}>
+                <AiOutlineClose />
+              </Show>
+            </>
+          }
+        >
+          {/* content for  unpressed state  */}
+          <Show when={!hasBeenPressed && !isError}>Copy Link</Show>
 
           {/* content for pressed and errorless state */}
-          <Show when={hasBeenPressed && !isError}>
-            <AiOutlineCheck />
-          </Show>
+          <Show when={hasBeenPressed && !isError}>Link Copied !</Show>
 
-          <Show when={hasBeenPressed && isError}>
-            <AiOutlineClose />
-          </Show>
-        </>
-      }
-    >
-      {/* content for  unpressed state  */}
-      <Show when={!hasBeenPressed}>Copy Link</Show>
+          {/* content for pressed and has error state */}
+          <Show when={isError}>Fail To Copy !</Show>
+        </ActionButton>
+      </Show>
 
-      {/* content for pressed and errorless state */}
-      <Show when={hasBeenPressed && !isError}>Link Copied !</Show>
+      <Show when={shareIsSupported === true}>
+        <ActionButton
+          disabled={hasBeenPressed}
+          className="animate-fade-in"
+          onClick={shareInfo}
+          color={isError ? "red" : "teal"}
+          icon={
+            <>
+              <Show when={!hasBeenPressed && !isError}>
+                <AiOutlineLink className="text-teal-500" />
+              </Show>
 
-      {/* content for pressed and has error state */}
-      <Show when={hasBeenPressed && isError}>Fail To Copy !</Show>
-    </ActionButton>
+              <Show when={isError}>
+                <AiOutlineClose />
+              </Show>
+            </>
+          }
+        >
+          {/* content for  unpressed state  */}
+          <Show when={!hasBeenPressed && !isError}>Share Page</Show>
+
+          {/* content for pressed and has error state */}
+          <Show when={isError}>Fail To Share !</Show>
+        </ActionButton>
+      </Show>
+    </>
   );
 }
