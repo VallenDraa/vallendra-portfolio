67354564b2cc44800785686c59caba5c6790aad8
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-01-30 20:35:02 +0700

    refactor(design): modified og image template

--- src/server/ogComponents/showcase.og.tsx
+++ src/server/ogComponents/showcase.og.tsx
@@ -5,17 +5,26 @@ export default function Showcase({ title, shortDesc, imgLink }: OgComponent) {
     <div tw="flex flex-col w-[1280px] h-[720px] font-sans justify-around">
       {/* top */}
       <div tw="flex items-center justify-between w-[1280px] text-white h-[630px] bg-[#212121] relative">
-        <div tw="absolute h-[400px] w-[400px] rounded-full bg-[#7986cb] opacity-100 top-[180px] right-[465px]" />
+        <div
+          tw="absolute h-[400px] w-[400px] rounded-full bg-[#7986cb] opacity-100 top-[100px] right-[465px]"
+          style={{ zIndex: "30" }}
+        />
+        <div
+          tw="absolute h-[400px] w-[400px] shadow-2xl rounded-full bg-[#ce93d8] opacity-100 top-[190px] right-[380px]"
+          style={{ zIndex: "0" }}
+        />
 
         {/* data */}
         <div tw="flex flex-col px-10 w-[520px] h-[630px] justify-center">
-          <p tw="text-6xl text-white mb-0 text-[#f48fb1]">{title}</p>
+          <p tw="text-5xl text-white mb-0 text-[#ffca28] opacity-90 font-bold">
+            {title}
+          </p>
           <p tw="text-xl text-white/80">{shortDesc}</p>
         </div>
 
         {/* image */}
         <img
-          tw="rounded w-5/6 self-end"
+          tw="rounded w-[700px] self-center shadow-2xl shadow-"
           src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${imgLink}.png`}
           alt={title}
         />
@@ -32,7 +41,7 @@ export default function Showcase({ title, shortDesc, imgLink }: OgComponent) {
             height={80}
           />
           <div tw="flex flex-col">
-            <span tw="text-base text-white/80 mb-0.5">
+            <span tw="text-base text-white/70 mb-0.5">
               Jestine Vallendra Dwi Putra
             </span>
             <span tw="text-sm text-white/60">Front-End Web Developer</span>
