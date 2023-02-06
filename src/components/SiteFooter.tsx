0b5c248f30bd9b970ebc54768a5ed605778a52c8
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-06 21:45:34 +0700

    refactor: changed the design of the site footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,18 +1,41 @@
 import { useMemo } from "react";
-import SocialsWithIcon from "./SocialWithIcons";
+import { FaHeart } from "react-icons/fa";
 
 export default function SiteFooter() {
   const year = useMemo(() => new Date().getFullYear(), []);
 
   return (
-    <footer className="gradient-underline gradient-underline--primary relative bg-indigo-50 before:!top-0 dark:bg-gray-900">
-      <div className="relative z-40 mx-auto flex max-w-screen-xl flex-col-reverse items-center justify-between gap-5 px-8 py-4 lg:flex-row 2xl:px-2">
-        <span className="text-indigo-500 dark:text-gray-400 lg:basis-1/3">
-          &copy; {year} VallenDra | Front-End Developer
-        </span>
+    <footer className="gradient-underline gradient-underline--primary relative before:!top-0">
+      <div className="relative z-40 mx-auto flex max-w-screen-xl flex-col items-center justify-between gap-2 px-8 py-6">
+        {/* portfolio tech stack */}
+        <div className="flex items-center gap-1 text-sm text-indigo-300 dark:text-gray-500">
+          <span>Built with</span>
+          <a
+            href="https://nextjs.org"
+            target="_blank"
+            className="underline underline-offset-2"
+          >
+            Next.js,
+          </a>
+          <a
+            href="https://www.material-tailwind.com/"
+            target="_blank"
+            className="underline underline-offset-2"
+          >
+            Material Tailwind,
+          </a>
+          <span className="flex items-center gap-1.5">
+            and
+            <span className="flex items-center">
+              <FaHeart className="translate-y-0.5 text-sm text-red-500" />.
+            </span>
+          </span>
+        </div>
 
-        <div className="flex grow items-center justify-center gap-2 lg:flex-grow-0 lg:basis-1/3 lg:justify-end">
-          <SocialsWithIcon size="text-4xl" />
+        <div className="flex items-center gap-2 text-indigo-500 dark:text-gray-400">
+          <span className="text-xs">
+            &copy; {year} VallenDra | Front-End Developer
+          </span>
         </div>
       </div>
     </footer>
