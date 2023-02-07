00d56e10b8bc7e42aca262528ee5de2ef1c0ab02
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-07 18:46:34 +0700

    fix: export an empty object for every file in unused folder

--- unused/BlurredBlob.tsx
+++ unused/BlurredBlob.tsx
@@ -1,23 +1,25 @@
-export default function BlurredBlob({
-  left = "",
-  top = "",
-  right = "",
-  bottom = "",
-  translateY = "",
-  translateX = "",
-  scale = "scale-110",
-}: {
-  left?: string;
-  top?: string;
-  right?: string;
-  bottom?: string;
-  translateX?: string;
-  translateY?: string;
-  scale?: string;
-}) {
-  return (
-    <div
-      className={`absolute h-[19rem] w-[19rem] rotate-12 skew-x-6 rounded-full bg-gradient-to-br from-indigo-200 to-pink-200 opacity-100 blur-3xl transition-transform duration-200 dark:from-indigo-700 dark:to-pink-700 dark:opacity-50 ${scale} ${left} ${right} ${top} ${bottom} ${translateX} ${translateY}`}
-    ></div>
-  );
-}
+// export default function BlurredBlob({
+//   left = "",
+//   top = "",
+//   right = "",
+//   bottom = "",
+//   translateY = "",
+//   translateX = "",
+//   scale = "scale-110",
+// }: {
+//   left?: string;
+//   top?: string;
+//   right?: string;
+//   bottom?: string;
+//   translateX?: string;
+//   translateY?: string;
+//   scale?: string;
+// }) {
+//   return (
+//     <div
+//       className={`absolute h-[19rem] w-[19rem] rotate-12 skew-x-6 rounded-full bg-gradient-to-br from-indigo-200 to-pink-200 opacity-100 blur-3xl transition-transform duration-200 dark:from-indigo-700 dark:to-pink-700 dark:opacity-50 ${scale} ${left} ${right} ${top} ${bottom} ${translateX} ${translateY}`}
+//     ></div>
+//   );
+// }
+
+export {}
--- unused/email/index.ts
+++ unused/email/index.ts
@@ -61,3 +61,5 @@
 // };
 
 // export default handler;
+
+export {}
