72f6e4298ee6c28119f07341076e2b87f59e9e0a
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-08 19:26:47 +0700

    feat: implemented offline fallback

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
 import ContextProviders from "../components/Layout/ContextProviders";
 import ProgressBar from "../components/ProgressBar";
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from "@vercel/analytics/react";
+import useNetworkStatus from "../utils/client/hooks/useNetworkStatus";
+import OfflinePage from "../components/OfflinePage";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,13 +18,15 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const SiteLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+  const { isOnline } = useNetworkStatus();
+
+  const SiteLayout = Component.getLayout ?? (page => <Layout>{page}</Layout>);
 
   return (
     <ContextProviders>
       <ProgressBar />
       <Analytics />
-      {SiteLayout(<Component {...pageProps} />)}
+      {isOnline ? SiteLayout(<Component {...pageProps} />) : <OfflinePage />}
     </ContextProviders>
   );
 }
