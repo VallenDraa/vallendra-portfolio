fecd7a7a4494115fa24faf6ad16baa1d3cc47018
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-11 11:42:24 +0700

    refactor: change select color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className="dark">
       <Head />
-      <body className="scrollbar-kece bg-indigo-50 caret-pink-300 overflow-x-hidden selection:bg-indigo-200/70 dark:bg-gray-900 dark:caret-pink-200 dark:selection:bg-indigo-400/70">
+      <body className="scrollbar-kece bg-indigo-50 caret-pink-300 overflow-x-hidden selection:bg-indigo-200/40 dark:bg-gray-900 dark:caret-pink-200 dark:selection:bg-indigo-400/40">
         <Main />
         <NextScript />
       </body>
