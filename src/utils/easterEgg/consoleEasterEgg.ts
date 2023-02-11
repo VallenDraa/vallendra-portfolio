2665ac5e5600c6606d16f05115c583a20bc15ad5
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-11 13:32:33 +0700

    feat: add new easter egg

---
+++ src/utils/easterEgg/consoleEasterEgg.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+export default function consoleMessage() {
+  console.log(
+    `
+    %c██   ██ ███████ ██    ██     ████████ ██   ██ ███████ ██████  ███████   ██
+    %c██   ██ ██       ██  ██         ██    ██   ██ ██      ██   ██ ██        ██
+    %c███████ █████     ████          ██    ███████ █████   ██████  █████     ██
+    %c██   ██ ██         ██           ██    ██   ██ ██      ██   ██ ██       
+    %c██   ██ ███████    ██           ██    ██   ██ ███████ ██   ██ ███████   ██
+    `,
+    "color: #f48fb1;",
+    "color: #f491b0;",
+    "color: #f598ac;",
+    "color: #f599ab;",
+    "color: #f6a0a7;",
+  );
+
+  console.log(
+    `
+   %cYou seemed interested in the code🤔, here is the repo link !
+    Also drop a ⭐ when you're visiting 😇
+    %chttps://github.com/VallenDraa/vallendra-portfolio
+    `,
+    "font-family: Segoe UI, sans-serif; font-size: 1rem; font-weight: 800; margin-top:0.2rem; color:#c5cae9",
+    "font-family: Segoe UI, sans-serif; font-size: 1rem; font-weight: 800; margin-top:1rem;",
+  );
+}
