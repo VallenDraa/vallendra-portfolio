870897279dca8ccb95806015799425b354d9eccc
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-11 14:31:19 +0700

    build: modify next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 export default {
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
   images: {
     domains: ["localhost", "vallendra.my.id", "vallendra-portfolio.vercel.app"],
