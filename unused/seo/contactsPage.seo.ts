012156e93590172db4ad4ba1302fc73e58f02881
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-14 12:43:37 +0700

    chore: organized unused files

---
+++ unused/seo/contactsPage.seo.ts
@@ -0,0 +1,24 @@
+// import { SEO } from "../../src/interfaces/seo.interface";
+// import { ogLinkMaker } from "../../src/utils/client/helpers/ogLinkMaker";
+
+// const contactsPageSeo: SEO = {
+//   base: {
+//     title: "VallenDra | Contacts",
+//     desc: "The page where visitor can contact vallendra through email or others means that are provided.",
+//   },
+//   og: {
+//     siteName: "Jestine Vallendra Dwi Putra",
+//     contentType: "contacts page",
+//     imageUrl: ogLinkMaker({
+//       title: "Contacts",
+//       shortDesc:
+//         "The page where visitor can contact vallendra through email or others means that are provided.",
+//       imgLink: "pages/contacts-page_mipiu7",
+//     }),
+//     imageAlt: "A screenshot of the contacts page",
+//     siteUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/contacts`,
+//   },
+// };
+
+// export default contactsPageSeo;
+export {};
