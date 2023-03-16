f4120d6e86f3fc1bb8d123438c8233ed8c56bbcf
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-03-16 09:18:17 +0700

    feat: update font size for my name in the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -81,7 +81,7 @@ export default function NavbarComponent() {
     >
       <div className="layout flex items-center justify-between">
         {/* name section */}
-        <span className="text-lg font-semibold text-zinc-700 dark:text-zinc-200 md:text-xl lg:basis-1/3">
+        <span className="text-xl font-semibold text-zinc-700 dark:text-zinc-200 lg:basis-1/3">
           <Link href="/">VallenDra</Link>
         </span>
 
