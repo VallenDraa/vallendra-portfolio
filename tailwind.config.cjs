92387a92a3949f793f8f90f25a0d4ca557f46306
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-03-29 16:23:02 +0700

    chore: update all deps and edit configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -71,8 +71,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
