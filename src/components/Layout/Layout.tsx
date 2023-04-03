71b780ff1b1eb1a98a2d493efa29437cd2c8fb3d
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-04-03 12:46:57 +0700

    fix: remove dynamic component and useRouter bugs in production

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,33 +1,15 @@
 import R from "react";
-import { useRouter } from "next/router";
-import Scrollbars from "react-custom-scrollbars-2";
 import dynamic from "next/dynamic";
 import NavbarComponent from "components/Navbar/Navbar";
 import BreathingBackground from "components/BreathingBackground";
 import SiteFooter from "components/Layout/SiteFooter/SiteFooter";
+import StyledTooltip from "components/StyledComponents/StyledTooltip";
 
 const GoToTopBtn = dynamic(() => import("components/GoToTopBtn"), {
   ssr: false,
 });
 
-const StyledTooltip = dynamic(
-  () => import("components/StyledComponents/StyledTooltip"),
-  { ssr: false },
-);
-
 export default function Layout({ children }: { children: R.ReactNode }) {
-  const router = useRouter();
-
-  const scrollbarRef = R.useRef<Scrollbars>(null);
-
-  /* scroll page to top on url change (except hash change)
-  ======================================================== */
-  R.useEffect(() => {
-    if (!router.asPath.includes("#")) {
-      scrollbarRef.current?.scrollToTop();
-    }
-  }, [router.asPath]);
-
   return (
     <div className="flex min-h-screen flex-col">
       <a
--- src/components/StyledComponents/StyledTooltip.tsx
+++ src/components/StyledComponents/StyledTooltip.tsx
@@ -1,19 +1,14 @@
 import type { ITooltip } from "react-tooltip";
-import dynamic from "next/dynamic";
+import { Tooltip } from "react-tooltip";
 
-const Tooltip = dynamic(
-  () => import("react-tooltip").then(mod => mod.Tooltip),
-  { ssr: false },
-);
+export const DEFAULT_DELAY = 250;
 
 export default function StyledTooltip(props: ITooltip) {
   return (
     <Tooltip
       place="bottom"
       {...props}
-      className="z-50 animate-fade-in !rounded-md !bg-indigo-50 !text-indigo-500 !shadow-sm !shadow-indigo-200 dark:!bg-zinc-800 dark:!text-zinc-200 dark:!shadow-zinc-600"
+      className="animate-fade-in z-50 !rounded-md !bg-indigo-50 !text-indigo-500 !shadow-sm !shadow-indigo-200 dark:!bg-zinc-800 dark:!text-zinc-200 dark:!shadow-zinc-600"
     />
   );
 }
-
-export const DEFAULT_DELAY = 250;
