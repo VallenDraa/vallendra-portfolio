3fcdbdfb4de4274a67850547f03b8170d81e5e93
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-04-13 18:46:55 +0700

    feat: add fancy transition for image placeholder

--- src/components/StyledComponents/CldImgWithBlur.tsx
+++ src/components/StyledComponents/CldImgWithBlur.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { CldImage, CldImageProps } from "next-cloudinary";
+import { Transition } from "@headlessui/react";
 import { useState } from "react";
-import Show from "utils/client/jsx/Show";
 
 type CldImgWithBlurProps = CldImageProps & {
   hasDynamicSize?: boolean;
@@ -10,9 +10,10 @@ type CldImgWithBlurProps = CldImageProps & {
 export default function CldImgWithBlur({
   className,
   hasDynamicSize = true,
+  src,
   ...props
 }: CldImgWithBlurProps) {
-  const [hasLoaded, setHasLoaded] = useState(false);
+  const [hasNotLoaded, setHasNotLoaded] = useState(true);
 
   return (
     <div
@@ -22,20 +23,30 @@ export default function CldImgWithBlur({
         "overflow-clip",
       )}
     >
-      <Show when={!hasLoaded}>
+      <Transition
+        show={hasNotLoaded}
+        enter="ease-out duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        className="absolute inset-0 z-20"
+      >
         <CldImage
           {...props}
+          src={src}
           quality={1}
-          height={144}
-          width={192}
+          height={72}
+          width={96}
           format="webp"
-          effects={[{ blur: "2000" }]}
-          className="absolute my-0 h-full w-full blur-sm"
+          className="my-0 h-full w-full blur-md"
         />
-      </Show>
+      </Transition>
 
       <CldImage
         {...props}
+        src={src}
         format="webp"
         sizes={
           hasDynamicSize
@@ -43,10 +54,10 @@ export default function CldImgWithBlur({
             : ""
         }
         className={clsx(
-          !className?.includes("absolute") ? "relative" : "absolute",
           "z-10 w-full",
+          !className?.includes("absolute") ? "relative" : "absolute",
         )}
-        onLoad={() => setHasLoaded(true)}
+        onLoad={() => setHasNotLoaded(false)}
       />
     </div>
   );
