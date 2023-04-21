b909f4f5a8d25672723821555a5fd55f4de69cf2
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-04-21 11:04:41 +0700

    feat: disable link when topProjectsItem is not highlighted

--- src/components/Home/TopProjects/TopProjectsItem.tsx
+++ src/components/Home/TopProjects/TopProjectsItem.tsx
@@ -52,7 +52,7 @@ export default function TopProjectsItem({
         "relative flex min-h-[500px] flex-col gap-0.5 border-t-2 border-dashed border-indigo-300/70 before:border-dashed after:border-dashed dark:border-white/40 lg:h-fit lg:items-center lg:gap-0 lg:rounded-tr-none",
         isLast && "border-b-2",
         twistDirection !== "left"
-          ? "border-l-2 pr-3 before:absolute before:inset-y-0 before:left-0 before:z-50 before:mx-auto before:hidden before:w-12 before:border-l-2 before:border-indigo-300/70 dark:before:border-white/40 lg:w-full lg:flex-row-reverse lg:justify-end lg:rounded-l-xl lg:border-0 lg:border-l-0 lg:before:left-1/2 lg:before:block lg:before:-translate-x-1/2 lg:before:rounded-tl-2xl before:lg:rounded-bl-2xl"
+          ? "border-l-2 pr-3 before:absolute before:inset-y-0 before:left-0 before:z-50 before:mx-auto before:hidden before:w-12 before:border-l-2 before:border-indigo-300/70 dark:before:border-white/40 lg:w-full lg:flex-row-reverse lg:justify-end lg:rounded-l-xl lg:border-0 lg:border-l-0 lg:before:left-1/2 lg:before:block lg:before:-translate-x-1/2 before:lg:rounded-bl-2xl lg:before:rounded-tl-2xl"
           : clsx(
               !isFirst && "lg:after:rounded-r-2xl",
               "border-r-2 pl-3 after:absolute after:inset-y-0 after:right-0 after:z-50 after:hidden after:w-12 after:border-r-2 after:border-indigo-300/70 dark:after:border-white/40 lg:flex-row lg:border-0 lg:border-r-0 lg:after:right-1/2 lg:after:block after:lg:rounded-br-2xl",
@@ -64,12 +64,12 @@ export default function TopProjectsItem({
 
       {/* div to hide the top-left border for smaller screen */}
       <Show when={isFirst}>
-        <div className="absolute left-0 right-[calc(50%-1px)] -top-3 block h-3.5 bg-[#E9EDFF] dark:bg-[#1E1E21] lg:hidden" />
+        <div className="absolute -top-3 left-0 right-[calc(50%-1px)] block h-3.5 bg-[#E9EDFF] dark:bg-[#1E1E21] lg:hidden" />
       </Show>
 
       {/* div to hide the bottom-right border for smaller screen */}
       <Show when={isLast}>
-        <div className="absolute right-0 left-[calc(50%-1px)] -bottom-3 block h-3.5 bg-[#E9EDFF] dark:bg-[#1E1E21] lg:hidden" />
+        <div className="absolute -bottom-3 left-[calc(50%-1px)] right-0 block h-3.5 bg-[#E9EDFF] dark:bg-[#1E1E21] lg:hidden" />
       </Show>
 
       {/* project number */}
@@ -117,7 +117,7 @@ export default function TopProjectsItem({
         className={clsx(
           !projectIsInView && "scale-90 grayscale",
           twistDirection === "left" && "lg:justify-end",
-          "relative z-40 flex basis-1/2 items-center pt-2 pb-8 transition duration-500 lg:p-0",
+          "relative z-40 flex basis-1/2 items-center pb-8 pt-2 transition duration-500 lg:p-0",
         )}
       >
         <div
@@ -163,8 +163,13 @@ export default function TopProjectsItem({
               href={`/projects/${project.slug}`}
               alwaysShowIcon={false}
               icon={<BsArrowRight className="text-indigo-400" />}
-              className="relative mt-2 flex w-full items-center justify-center gap-2 self-start border !border-indigo-400 py-3 px-4 !text-indigo-400 hover:bg-indigo-500/10 lg:w-max"
-              color="indigo"
+              className={clsx(
+                "relative",
+                "!text-indigo-400",
+                !projectIsInView && "pointer-events-none",
+                "mt-2 flex w-full items-center justify-center gap-2 self-start px-4 py-3 lg:w-max",
+                "border !border-indigo-400  hover:bg-indigo-500/10",
+              )}
             >
               See Project
             </StyledButton>
